a3a039fce3b00ec5f873de161f5b04e63045d1c5
ANNANYA DEVI <annanyadevi18@gmail.com>
2024-07-19 23:52:42 +0530

    Add files via upload

---
+++ java_script/script4.js
@@ -0,0 +1,40 @@
+let btn=document.getElementById("btn");
+console.log(btn);
+btn.addEventListener("click",()=>
+{
+    // alert("hello ")
+    document.querySelector(".box").innerHTML="<p>hiiiiiiii all <b>annanya here </b></p>";
+});
+btn.addEventListener("dblclick", () => {
+    const rndCol = `rgb(${Math.floor(Math.random() * 256)} ${Math.floor(Math.random() *256)} ${Math.floor(Math.random() * 256)})`;
+    console.log(rndCol);
+    document.body.style.backgroundColor =rndCol;
+  });
+  btn.addEventListener("contextmenu",()=>
+{
+    //right click
+    document.querySelector(".box").style.backgroundColor="red";
+});
+
+
+const password = document.querySelector('input[type="password"]');
+password.addEventListener("focus", (e) => {
+  event.target.style.background = "pink";
+});
+password.addEventListener("blur", (e) => {
+  event.target.style.background = "aqua";
+});
+document.addEventListener("keydown",(e)=>
+{
+    //keyup
+    console.log(e.key,"  ",e.keyCode);
+});
+let division=document.querySelector(".new");
+division.addEventListener("mouseenter",()=>
+{
+    division.style.backgroundColor="aqua";
+});
+division.addEventListener("mouseleave",()=>
+    {
+        division.style.backgroundColor="tomato";
+    });
